7713c3d9eb6bba33989ef2ebcfafc43e056ac4f1
Jia Liu <proljc@gmail.com>
2026-09-24 01:34:10 +0800

    Add sparkle
    Signed-off-by: Jia Liu <proljc@gmail.com>

--- Package.swift
+++ Package.swift
@@ -8,9 +8,15 @@ import PackageDescription
 let package = Package(
     name: "Keystarter",
     platforms: [.macOS(.v13)],
+    dependencies: [
+        .package(url: "https://github.com/sparkle-project/Sparkle", from: "2.6.0")
+    ],
     targets: [
         .executableTarget(
             name: "Keystarter",
+            dependencies: [
+                .product(name: "Sparkle", package: "Sparkle")
+            ],
             path: "Sources/Keystarter",
             resources: [
                 .copy("../../Resources/Compiled/Keystarter.icns"),
